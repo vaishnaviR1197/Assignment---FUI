d1b36e1c3a2502d1dc5c3a33c4a70715220f56a7
Vaishnavi Rajasekaran <90331639+vaishnaviR1197@users.noreply.github.com>
2021-11-19 09:23:01 +0530

    Add files via upload

---
+++ Assignment3/calculator.js
@@ -0,0 +1,5 @@
+const cal = require('./calc');
+console.log("Addition: " + cal.add(20, 40));
+console.log("Subtraction: " + cal.sub(20, 40));
+console.log("Multiplication: " + cal.mul(20, 40));
+console.log("Division: " + cal.div(20, 40));
